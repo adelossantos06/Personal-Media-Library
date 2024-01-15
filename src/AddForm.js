48ad920101a9b96e33d41a44fb843ea0ee1f0686
Alyssa De Los Santos <adelossantos6@gmail.com>
2024-01-15 11:39:22 -0600

    add form details

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import NavBar from './NavBar'
+import "./AddForm.css"
 
 function AddForm() {
     return (
-        <div>
-            <NavBar />
-        </div>
+        <>
+            <div>
+                <NavBar />
+            </div>
+            <form>
+                <label>Title:</label>
+                <input type="text" ></input>
+                <br />
+                <label>Genre:</label>
+                <input type="text" ></input>
+                <br />
+                <label>Liked:</label>
+                <select>
+                    <option value="Yes" >Yes</option>
+                    <option value="No" >No</option>
+                </select>
+                <br />
+                <label>Review:</label>
+                <input type="text" ></input>
+                <br />
+                <label>Category:</label>
+                <select>
+                    <option>Movies</option>
+                    <option>TV Shows</option>
+                    <option>Books</option>
+                    <option>Podcasts</option>
+                </select>
+                <br />
+                <input type="submit" value="Submit"></input>
+
+            </form>
+        </>
     )
 }
 

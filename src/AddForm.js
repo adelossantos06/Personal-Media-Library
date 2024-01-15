ab0dcf8f3f95b3be0f70b7f0c36c8db13972a8d4
Alyssa De Los Santos <adelossantos6@gmail.com>
2024-01-15 15:44:07 -0600

    add form css

--- src/AddForm.js
+++ src/AddForm.js
@@ -8,33 +8,38 @@ function AddForm() {
             <div>
                 <NavBar />
             </div>
-            <form>
-                <label>Title:</label>
-                <input type="text" ></input>
-                <br />
-                <label>Genre:</label>
-                <input type="text" ></input>
-                <br />
-                <label>Liked:</label>
-                <select>
-                    <option value="Yes" >Yes</option>
-                    <option value="No" >No</option>
-                </select>
-                <br />
-                <label>Review:</label>
-                <input type="text" ></input>
-                <br />
-                <label>Category:</label>
-                <select>
-                    <option>Movies</option>
-                    <option>TV Shows</option>
-                    <option>Books</option>
-                    <option>Podcasts</option>
-                </select>
-                <br />
-                <input type="submit" value="Submit"></input>
+            <div className="formDiv">
+                <form>
+                    <label>Title:</label>
+                    <input type="text" ></input>
+                    <br />
+                    <label>Image:</label>
+                    <input type="text" ></input>
+                    <br />
+                    <label>Genre:</label>
+                    <input type="text" ></input>
+                    <br />
+                    <label>Liked:</label>
+                    <select className="dropdown">
+                        <option value="Yes" >Yes</option>
+                        <option value="No" >No</option>
+                    </select>
+                    <br />
+                    <label>Review:</label>
+                    <textarea className='review' ></textarea>
+                    <br />
+                    <label>Category:</label>
+                    <select className="dropdown">
+                        <option>Movies</option>
+                        <option>TV Shows</option>
+                        <option>Books</option>
+                        <option>Podcasts</option>
+                    </select>
+                    <br />
+                    <input className="submitButton" type="submit" value="Submit"></input>
 
-            </form>
+                </form>
+            </div>
         </>
     )
 }
